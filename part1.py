d58bb2fc5fb937f5d0a60f7129e997018a6b6931
asakinory <asakinory@gmail.com>
2023-03-16 08:21:31 +0200

    Initiating code files

--- part1.py
+++ part1.py
@@ -1,5 +1,5 @@
 import random
-def start(args, hkubeapi):
+def part1(args, hkubeapi):
     print('algorithm: range start')
     input = args['input'][0]
     array = [random.random() for _ in range(input)]
--- part2.py
+++ part2.py
@@ -1,4 +1,4 @@
-def start(args, hkubeapi):
+def part2(args, hkubeapi):
     print('algorithm: range start')
     input = args['input'][0]
     array = [i*2.2 for i in input]
